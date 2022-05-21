59d0444f67b172f1050db884d442b4126ae9ae23
John Benninghoff <jbenninghoff@mac.com>
2022-05-21 17:17:42 -0500

    Add test functions

---
+++ R/functions.R
@@ -0,0 +1,15 @@
+#' @title Title
+#' @details Details
+#' ```
+#' 1+1
+#' ```
+#' @md
+foo <- function() NULL
+
+#' @title Title
+#' @details Details
+#' ```r
+#' 1+1
+#' ```
+#' @md
+bar <- function() NULL
